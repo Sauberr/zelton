5f451b9f01f8c80d8cafe8ef0ad3b09d63fff8b3
Toma <greebsasha24@gmail.com>
2024-03-21 18:07:35 +0300

    start filter by smt

--- ecomprj/core/context_processors.py
+++ ecomprj/core/context_processors.py
@@ -1,10 +1,12 @@
-from core.models import Product, Category, Address
+from core.models import Category, Address, Vendor
 
 
 def default(request):
     categories = Category.objects.all()
+    vendors = Vendor.objects.all()
+
     try:
         address = Address.objects.get(user=request.user)
     except:
         address = None
-    return {'categories': categories, 'address': address}
+    return {'categories': categories, 'address': address, 'vendors': vendors}
