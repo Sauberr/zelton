8f3abce5af8c5b90d94dec05117470d9c7fccea1
Toma <greebsasha24@gmail.com>
2024-04-17 16:12:12 +0300

    fix bags add product in admin dashboard

--- ecomprj/useradmin/urls.py
+++ ecomprj/useradmin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from useradmin.views import dashboard
+from useradmin.views import dashboard, products
 
 app_name = "useradmin"
 
 urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
+    path("products/", products, name="products"),
 ]
